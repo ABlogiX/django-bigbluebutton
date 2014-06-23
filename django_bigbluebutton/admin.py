da0c8dc58c8f3793103526b99b56b98ccdd508f8
Alexandre Papin <alexandre@alexandre.papin@ablogix.fr>
2014-06-23 16:15:40 +0200

    I'm not sure about file organization.

--- django_bigbluebutton/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django_bigbluebutton.models import Meeting
-from django_bigbluebutton.models import RegisteredUser
-
-admin.site.register(Meeting)
-admin.site.register(RegisteredUser)
--- django_bigbluebutton/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django_bigbluebutton.views import MeetingsView
-
-urlpatterns = patterns('',
-    url(r'^', MeetingsView.as_view(), name='meetings'),
-)
