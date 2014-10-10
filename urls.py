7718790a094ea3f6d57d8dc7eb8175f36de88776
Alexandre Papin <alexandre.papin@ablogix.fr>
2014-10-10 15:40:50 +0200

    Add URLs for the new views.

--- urls.py
+++ urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+from django_bigbluebutton.views import MeetingSubscriptionView
+from django_bigbluebutton.views import MeetingConnectionView
 from django_bigbluebutton.views import MeetingsView
 
-urlpatterns = patterns(
-    '',
+urlpatterns = patterns(settings.DJANGO_BBB_BASE_URL,
+    url(r'^(\d+)/{}'.format(settings.DJANGO_BBB_SUBSCRIPTION_URL),
+        MeetingSubscriptionView.as_view(), name='meeting_subscription'),
+    url(r'^(\d+)/{}'.format(settings.DJANGO_BBB_CONNECTION_URL),
+        MeetingConnectionView.as_view(), name='meeting_connection'),
     url(r'^', MeetingsView.as_view(), name='meetings'),
 )
